070c4f1defe2d822b1095cd271fe1ae88ba02f32
Kewbin <42474093+Kewbin@users.noreply.github.com>
2019-08-06 18:48:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-token = 'NTUxMzIyODYwODc1MTUzNDE4.XUmq2w.-XiVt7-izMOi_XKSErNXNNfB7co'
+from cryptography.fernet import Fernet
+
+
+key = 'Pmgzg9fQGFXnA4bUAf1yubBDQBjSYDWqZ7ERrfIYZa4='
+text = 'gAAAAABdSatcvDpw4kAvED8ZohBtnIdL_NBsKaBUGY8MYJKZfaJsHdEDU4R33nmuyo1bCgSC3NXQI2PMgfH4n5edCLEunEByNC5Wz24riU48sIeBTk7dJpiAtuzN54X2E2QJqioEroNV_nQ72z70PW_HNANGnnoKUQ=='
+f = Fernet(key)
+token = f.decrypt(text.encode()).decode("utf-8").replace('\n','') 
+
+
